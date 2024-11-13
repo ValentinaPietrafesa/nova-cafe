c833cb0b63915e14ca9addb027bf5535f6aab7f2
ValentinaPietrafesa <valentinapietrafesa03@gmail.com>
2024-11-13 16:44:34 -0300

    Add files via upload

---
+++ js/users.js
@@ -0,0 +1,34 @@
+// const users = [
+//   {
+//     id: 1,
+//     name: 'Martín Segovia',
+//     description:
+//       'Excelente lugar y atención, el café como siempre exquisito. Recomiendo la chocotorta y el chipá.',
+//   },
+//   {
+//     id: 2,
+//     name: 'Belen Mariani',
+//     description:
+//       'Riquísimo! Siempre impecable. El café es de los más ricos que probé!',
+//   },
+//   {
+//     id: 3,
+//     name: 'Mariano Cervantes',
+//     description:
+//       'Excelente lugar para hacer un break del día y tomar y/o comer algo rico! Muy buena atención.',
+//   },
+// ];
+
+// const $containerUsers = document.getElementById('container-users');
+
+// users.forEach((user) => {
+//   $containerUsers.innerHTML += ` <div class="col-md-4">
+//     <img
+//       src="https://via.placeholder.com/100"
+//       alt="Martín Segovia"
+//       class="img-fluid mb-3"
+//     />
+//     <h4>Martín Segovia</h4>
+//     <p>Excelente lugar y atención, el café como siempre exquisito.</p>
+//   </div>`;
+// });
